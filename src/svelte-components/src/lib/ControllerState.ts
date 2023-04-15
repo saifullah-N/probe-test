278cd33db7d4d9a70dc7ea8c0ab47500448cfec0
saifullah-N <n.mmsaifi16331@gmail.com>
2023-04-15 15:01:59 +0530

    filter states

--- src/svelte-components/src/lib/ControllerState.ts
+++ src/svelte-components/src/lib/ControllerState.ts
@@ -12,27 +12,27 @@ export const probingComplete = writable(false);
 export function handleControllerStateUpdate(state: Record<string, any>) {
     if (get(probingActive)) {
         if (state.pw === 0) {
-            PUT("write-log",{handleControllerStateUpdate:state});
+            PUT("write-log",{handleControllerStateUpdate:[state.pw ,state.log?.msg , state.cycle]});
             probeContacted.set(true);
             PUT("write-log",{probeContacted:true});
             
         }
         
         if (state.log?.msg === "Switch not found") {
-            PUT("write-log",{handleControllerStateUpdate:state});
+            PUT("write-log",{handleControllerStateUpdate:[state.pw ,state.log?.msg , state.cycle]});
             probingFailed.set(true);
         }
         
         if (state.cycle !== "idle") {
             PUT("write-log",{probingFailed:true , cycle:"idle"});
-            PUT("write-log",{handleControllerStateUpdate:state});
+            PUT("write-log",{handleControllerStateUpdate:[state.pw ,state.log?.msg , state.cycle]});
             probingStarted.set(true);
         }
         
         if (state.cycle === "idle" && get(probingStarted)) {
             probingStarted.set(false);
             probingComplete.set(true);
-            PUT("write-log",{handleControllerStateUpdate:state});
+            PUT("write-log",{handleControllerStateUpdate:[state.pw ,state.log?.msg , state.cycle]});
             PUT("write-log",{probingStarted:false , probingComplete:true , cycle:"idle"});
         }
     }
