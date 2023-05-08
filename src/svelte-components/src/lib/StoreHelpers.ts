4b5b6b5f9fc43dca9fb4480ac437374b9ab4e1a3
saifullah-N <n.mmsaifi16331@gmail.com>
2023-05-08 16:02:56 +0530

    probe log in storHelpers

--- src/svelte-components/src/lib/StoreHelpers.ts
+++ src/svelte-components/src/lib/StoreHelpers.ts
@@ -4,8 +4,13 @@ export function listenForChange<T>(writable: Writable<T>, cb: (value: T) => void
     const priorValue = get(writable);
 
     const unsubscribe = writable.subscribe((value) => {
+        console.log({value , priorValue}, typeof(value))
+
         if (value !== priorValue) {
+            console.log("inside if " , {value ,  priorValue} );
+            
             unsubscribe();
+
             cb(value);
         }
     });
@@ -15,6 +20,8 @@ export function waitForChange<T>(writable: Writable<T>): Promise<T> {
     console.log("waiting for change: ",writable );
 
     return new Promise((resolve) => {
-        listenForChange(writable, (value) => resolve(value));
+        listenForChange(writable, (value) => resolve(value)); 
     });
 }
+https://stackoverflow.com/questions/64862161/svelte-store-function-update?rq=1
+https://stackoverflow.com/questions/64862161/svelte-store-function-update?rq=1
